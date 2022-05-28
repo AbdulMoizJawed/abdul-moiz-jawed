c3fade54e26606353cc8dac067a9bb2bcbf0e7e6
AbdulMoizJawed <86627657+AbdulMoizJawed@users.noreply.github.com>
2022-05-28 16:25:51 -0700

    Home Component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
+// import About from "./About/About";
+// import Projects from "./Projects/Projects";
+// import Contact from "./Contact/Contact";
+// import ScrollToTop from "./ScrollToTop/ScrollToTop";
 function App() {
   return (
-
-  <div>
-  hi
-  </div>
+    <div>
+      <Router>
+        <div className="noise"></div>
+        <div className="App">
+          {/* <ScrollToTop /> */}
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            {/* <Route path="/about" exact element={<About />} />
+            <Route path="/projects" exact element={<Projects />} />
+            <Route path="/contact" exact element={<Contact />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
