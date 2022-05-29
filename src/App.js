42f9ae62fa3490b54b57f75bfb7528edcfd8e45a
AbdulMoizJawed <86627657+AbdulMoizJawed@users.noreply.github.com>
2022-05-29 07:47:20 -0700

    Removed All from Surge

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home/Home";
-import About from "./About/About";
-import Projects from "./Projects/Projects";
-import Contact from "./Contact/Contact";
-import ScrollToTop from "./ScrollToTop/ScrollToTop";
+
+
 function App() {
   return (
     <div>
-      {/* <Router> */}
-      <div className="noise"></div>
-      <div className="App">
-        <ScrollToTop />
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
-
-        {/* <Routes> */}
-        {/* <Route path="/" exact element={<Home />} /> */}
-        {/* <Route path="/about" exact element={<About />} />
-            <Route path="/projects" exact element={<Projects />} />
-            <Route path="/contact" exact element={<Contact />} /> */}
-        {/* </Routes> */}
-      </div>
-      {/* </Router> */}
+    
+     
     </div>
   );
 }
